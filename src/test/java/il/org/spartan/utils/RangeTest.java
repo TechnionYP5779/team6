8d83eaae53ca6702a036bb216524c72a5ce8a750
Michael Shohat <michaelsho@campus.technion.ac.il>
2018-11-03 15:48:03 +0200

    Range Test - includedIn

--- src/test/java/il/org/spartan/utils/RangeTest.java
+++ src/test/java/il/org/spartan/utils/RangeTest.java
@@ -15,4 +15,18 @@ public class RangeTest {
     assertFalse(r1.equals(r4));
     assertFalse(r3.equals(r4));
   }
+  
+  @SuppressWarnings("static-method") @Test public void RangeIncludenInTest() {
+    Range r1 = new Range(1,4);
+    Range r2 = new Range(1,4);
+    Range r3 = new Range(0,4);
+    Range r4 = new Range(1,5);
+    Range r5 = new Range(2,4);
+    Range r6 = new Range(1,3);
+    assertTrue(r1.includedIn(r2));
+    assertFalse(r3.includedIn(r1));
+    assertFalse(r4.includedIn(r1));
+    assertTrue(r5.includedIn(r1));
+    assertTrue(r6.includedIn(r1));
+  }
 }
