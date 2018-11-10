27dfe89cb282a790bc5004191a56aa759e1be8b5
eyal <eyalf210@gmail.com>
2018-11-10 18:10:10 +0200

    moved more tests fro as class. issue #59

--- src/test/java/fluent/ly/asTest.java
+++ src/test/java/fluent/ly/asTest.java
@@ -1,10 +1,17 @@
 package fluent.ly;
 
+import static fluent.ly.azzert.is;
 import static org.junit.Assert.*;
 
 import java.util.*;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.*;
+
+/** A static nested class hosting unit tests for the nesting class Unit test for
+ * the containing class.
+ **/
 
 public class asTest {
   @Test public void asTestEmptyIterator() {
@@ -84,5 +91,40 @@ public class asTest {
     final Object obj = new Object();
     res = as.string(obj);
     assertEquals(obj.toString(), res);
+  }
+
+  @Test public void asBitOfFalse() {
+    azzert.that(as.bit(false), is(0));
+  }
+
+  @Test public void asBitOfTrue() {
+    azzert.that(as.bit(true), is(1));
+  }
+
+  @Test public void asIntArraySimple() {
+    final int @NotNull [] is = as.intArray(100, 200, 200, 12, 13, 0);
+    assertArrayEquals(is, as.intArray(as.ingeterList(is)));
+  }
+
+  @Test public void asListSimple() {
+    // direct call `as.list(12, 13, 14)` kills Travis --or
+    final @NotNull List<Integer> is = as.list(new int @NotNull [] { 12, 13, 14 });
+    azzert.that(is.get(0), is(fluent.ly.box.it(12)));
+    azzert.that(is.get(1), is(fluent.ly.box.it(13)));
+    azzert.that(is.get(2), is(fluent.ly.box.it(14)));
+    azzert.that(is.size(), is(3));
+  }
+
+  @Test public void stringOfNull() {
+    azzert.that(as.string(null), is("null"));
+  }
+
+  @Test public void stringWhenToStringReturnsNull() {
+    azzert.that(as.string(new Object() {
+      @Override @Nullable
+      public String toString() {
+        return null;
+      }
+    }), is("null"));
   }
 }
