81dbd75b7ef7a78b63eca5f40604b3be8bff4167
eyal <eyalf210@gmail.com>
2018-11-10 18:31:56 +0200

    moved idiomatic tests. issue #59

--- src/test/java/fluent/ly/idiomaticTest.java
+++ src/test/java/fluent/ly/idiomaticTest.java
@@ -1,10 +1,14 @@
 package fluent.ly;
 
+import static fluent.ly.azzert.is;
+import static fluent.ly.idiomatic.take;
+import static fluent.ly.idiomatic.unless;
+import static fluent.ly.idiomatic.when;
 import static org.junit.Assert.*;
 
 import org.junit.*;
 
-// I admit, there is alot i don't understand in the idiomatic class...
+
 public class idiomaticTest {
   @SuppressWarnings("static-method") @Test public void incaseTest() {
     assertTrue(Integer.valueOf(7).equals(idiomatic.incase(3 > 2, Integer.valueOf(7))));
@@ -17,6 +21,57 @@ public class idiomaticTest {
     assertTrue(idiomatic.quote("my name is").equals("\'my name is\'"));
     assertTrue(idiomatic.quote("\'slim shady\'").equals("\'\'slim shady\'\'"));
   }
-  // some of the class alrady has tests inside the class, as i said, i dont
-  // really understand most of the stuff there atm ...
+
+
+  @Test public void use0() {
+    assert new idiomatic.Storer<>(this) != null;
+  }
+
+  @Test public void use08() {
+    azzert.isNull(unless(true).eval(() -> new Object()));
+  }
+
+  @Test public void use09() {
+    assert unless(false).eval(() -> new Object()) != null;
+  }
+
+  @Test public void use1() {
+    assert new idiomatic.Storer<>(this) != null;
+    new idiomatic.Storer<>(this).when(true);
+  }
+
+  @Test public void use10() {
+    assert when(true).eval(() -> new Object()) != null;
+  }
+
+  @Test public void use11() {
+    azzert.isNull(when(false).eval(() -> new Object()));
+  }
+
+  @Test public void use2() {
+    assert take(this) != null;
+    azzert.isNull(take(this).when(false));
+  }
+
+  @Test public void use3() {
+    azzert.that(take(this).when(true), is(this));
+  }
+
+  @Test public void use4() {
+    azzert.isNull(take(this).when(false));
+  }
+
+  @Test public void use5() {
+    azzert.that(take(this).unless(false), is(this));
+  }
+
+  @Test public void use6() {
+    azzert.isNull(take(this).unless(true));
+  }
+
+  @Test public void use7() {
+    azzert.isNull(take(this).unless(true));
+    azzert.isNull(take(null).unless(true));
+    azzert.isNull(take(null).unless(false));
+  }
 }
