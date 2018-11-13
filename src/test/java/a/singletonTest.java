0986db6e2d919c3423ff1e838ee2261d3f6e502b
elizabethyeshaayahu320 <44428417+elizabethyeshaayahu320@users.noreply.github.com>
2018-11-13 18:11:38 +0200

    Update singletonTest.java

--- src/test/java/a/singletonTest.java
+++ src/test/java/a/singletonTest.java
@@ -1,32 +1,29 @@
 package a;
 
-
-
-
-
-import static org.junit.Assert.*;
-
 import java.util.*;
 
 import org.junit.*;
 
+import static fluent.ly.azzert.*;
+
 @SuppressWarnings({ "static-method", "boxing" }) public class singletonTest {
- @Test public void list() {
-    assertEquals(Collections.emptyList(), singleton.list(null));
+ @SuppressWarnings("null") @Test public void list() {
+    assertCollectionsEqual(Collections.emptyList(), singleton.list(null));
     // T=int:
-    assertEquals(Collections.singletonList(5), singleton.list(5));
+    assertCollectionsEqual(Collections.singletonList(5), singleton.list(5));
     // T=string:
-    assertEquals(Collections.singletonList("555"), singleton.list("555"));
+    assertCollectionsEqual(Collections.singletonList("555"), singleton.list("555"));
     final Integer[] arr123 = new Integer[] { 1, 2, 3 };
-    assertEquals(Collections.singletonList(arr123), singleton.list(arr123));
+    assertCollectionsEqual(Collections.singletonList(arr123), singleton.list(arr123));
   }
 
   @Test public void array() {
-    Assert.assertArrayEquals(new Integer[] { null }, singleton.array(null));
+    assertArrayEquals(new Integer[] { null }, singleton.array(null));
     // T=int:
-    Assert.assertArrayEquals(Collections.singletonList(5).toArray(), singleton.array(5));
+    assertArrayEquals(Collections.singletonList(5).toArray(), singleton.array(5));
     // T=string:
-    Assert.assertArrayEquals(Collections.singletonList("555").toArray(), singleton.array("555"));
-    Assert.assertArrayEquals(Collections.singletonList(new Integer[] { 1, 2, 3 }).toArray(), singleton.array(new Integer[] { 1, 2, 3 }));
+    assertArrayEquals(Collections.singletonList("555").toArray(), singleton.array("555"));
+    assertArrayEquals(Collections.singletonList(new Integer[] { 1, 2, 3 }).toArray(), singleton.array(new Integer[] { 1, 2, 3 }));
   }
 }
+
