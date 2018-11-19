c55862368dc1754dc74cfaf8cc85ec78f6ed4f2d
HadadIdan <idan.hadad@campus.technion.ac.il>
2018-11-19 20:59:47 +0200

    refactor usage of assert to azzert in BoolTest, boxTest , close #17

--- src/test/java/fluent/ly/boxTest.java
+++ src/test/java/fluent/ly/boxTest.java
@@ -5,7 +5,7 @@ import org.junit.*;
 public class boxTest {
   @Test @SuppressWarnings("static-method") public void boxBoolTest() {
     final boolean a = true;
-    assert Box.box(a).booleanValue() == a;
+    azzert.that(Box.box(a).booleanValue(),azzert.is(a));
     assert !Box.box(false).booleanValue();
   }
 
@@ -13,90 +13,90 @@ public class boxTest {
     final boolean[] a = { true, false };
     final Boolean[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].booleanValue() == a[¢];
+      azzert.that(boxa[¢].booleanValue() ,azzert.is(a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void boxByteTest() {
     final byte a = Byte.parseByte("123", 8);
-    assert Box.box(a).byteValue() == a;
+    azzert.that( Box.box(a).byteValue() ,azzert.is(a));
   }
 
   @Test @SuppressWarnings("static-method") public void boxByteArrayTest() {
     final byte[] a = { Byte.parseByte("123", 8), Byte.parseByte("124", 8) };
     final Byte[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].byteValue() == a[¢];
+      azzert.that( boxa[¢].byteValue(),azzert.is(a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void boxCharacterTest() {
-    assert Box.box('a').charValue() == 'a';
+    azzert.that(Box.box('a').charValue(),azzert.is('a'));
   }
 
   @Test @SuppressWarnings("static-method") public void boxCharacterArrayTest() {
     final char[] a = { 'a', 'b' };
     final Character[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].charValue() == a[¢];
+      azzert.that( boxa[¢].charValue(),azzert.is(a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void boxDoubleTest() {
-    assert Box.box(5.7).doubleValue() == 5.7;
+    azzert.that( Box.box(5.7).doubleValue() ,azzert.is( 5.7));
   }
 
   @Test @SuppressWarnings("static-method") public void boxDoubleArrayTest() {
     final double[] a = { 5.7, 3.33 };
     final Double[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].doubleValue() == a[¢];
+      azzert.that( boxa[¢].doubleValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void boxFloatTest() {
-    assert Box.box(5).floatValue() == 5;
+    azzert.that( Box.box((float)5).floatValue() ,azzert.is( (float)5));
   }
 
   @Test @SuppressWarnings("static-method") public void boxFloatArrayTest() {
     final float[] a = { 5, 9 };
     final Float[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].floatValue() == a[¢];
+      azzert.that( boxa[¢].floatValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void boxIntegerTest() {
-    assert Box.box(3).intValue() == 3;
+    azzert.that( Box.box(3).intValue() ,azzert.is( 3));
   }
 
   @Test @SuppressWarnings("static-method") public void boxIntegerArrayTest() {
     final int[] a = { 3, 1 };
     final Integer[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].intValue() == a[¢];
+      azzert.that( boxa[¢].intValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void boxLongTest() {
-    assert Box.box(1232144).longValue() == 1232144;
+    azzert.that( Box.box(1232144L).longValue() ,azzert.is(1232144L));
   }
 
   @Test @SuppressWarnings("static-method") public void boxLongArrayTest() {
     final long[] a = { 1232144, 12312 };
     final Long[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].longValue() == a[¢];
+      azzert.that( boxa[¢].longValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void boxShortTest() {
-    assert Box.box(96).shortValue() == 96;
+    azzert.that( Box.box(96).shortValue() ,azzert.is( 96));
   }
 
   @Test @SuppressWarnings("static-method") public void boxShortArrayTest() {
     final short[] a = { 96, 12 };
     final Short[] boxa = Box.box(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert boxa[¢].shortValue() == a[¢];
+      azzert.that( boxa[¢].shortValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itBoolTest() {
     final boolean a = true;
-    assert Box.it(a).booleanValue() == a;
+    azzert.that( Box.it(a).booleanValue() ,azzert.is( a));
     assert !Box.it(false).booleanValue();
   }
 
@@ -104,84 +104,84 @@ public class boxTest {
     final boolean[] a = { true, false };
     final Boolean[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].booleanValue() == a[¢];
+      azzert.that( ita[¢].booleanValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itByteTest() {
     final byte a = Byte.parseByte("123", 8);
-    assert Box.it(a).byteValue() == a;
+    azzert.that( Box.it(a).byteValue() ,azzert.is( a));
   }
 
   @Test @SuppressWarnings("static-method") public void itByteArrayTest() {
     final byte[] a = { Byte.parseByte("123", 8), Byte.parseByte("124", 8) };
     final Byte[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].byteValue() == a[¢];
+      azzert.that( ita[¢].byteValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itCharacterTest() {
-    assert Box.it('a').charValue() == 'a';
+    azzert.that( Box.it('a').charValue() ,azzert.is( 'a'));
   }
 
   @Test @SuppressWarnings("static-method") public void itCharacterArrayTest() {
     final char[] a = { 'a', 'b' };
     final Character[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].charValue() == a[¢];
+      azzert.that( ita[¢].charValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itDoubleTest() {
-    assert Box.it(5.7).doubleValue() == 5.7;
+    azzert.that( Box.it(5.7).doubleValue() ,azzert.is( 5.7));
   }
 
   @Test @SuppressWarnings("static-method") public void itDoubleArrayTest() {
     final double[] a = { 5.7, 3.33 };
     final Double[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].doubleValue() == a[¢];
+      azzert.that( ita[¢].doubleValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itFloatTest() {
-    assert Box.it(5).floatValue() == 5;
+    azzert.that( Box.it((float)5).floatValue() ,azzert.is((float) 5));
   }
 
   @Test @SuppressWarnings("static-method") public void itFloatArrayTest() {
     final float[] a = { 5, 9 };
     final Float[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].floatValue() == a[¢];
+      azzert.that( ita[¢].floatValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itIntegerTest() {
-    assert Box.it(3).intValue() == 3;
+    azzert.that( Box.it(3).intValue() ,azzert.is( 3));
   }
 
   @Test @SuppressWarnings("static-method") public void itIntegerArrayTest() {
     final int[] a = { 3, 1 };
     final Integer[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].intValue() == a[¢];
+      azzert.that( ita[¢].intValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itLongTest() {
-    assert Box.it(1232144).longValue() == 1232144;
+    azzert.that( Box.it(1232144L).longValue() ,azzert.is(1232144L));
   }
 
   @Test @SuppressWarnings("static-method") public void itLongArrayTest() {
     final long[] a = { 1232144, 12312 };
     final Long[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].longValue() == a[¢];
+      azzert.that( ita[¢].longValue() ,azzert.is( a[¢]));
   }
 
   @Test @SuppressWarnings("static-method") public void itShortTest() {
-    assert Box.it(96).shortValue() == 96;
+    azzert.that( Box.it(96).shortValue() ,azzert.is( 96));
   }
 
   @Test @SuppressWarnings("static-method") public void itShortArrayTest() {
     final short[] a = { 96, 12 };
     final Short[] ita = Box.it(a);
     for (int ¢ = 0; ¢ < 2; ++¢)
-      assert ita[¢].shortValue() == a[¢];
+      azzert.that( ita[¢].shortValue() ,azzert.is( a[¢]));
   }
 }
--- src/test/java/il/org/spartan/utils/BoolTest.java
+++ src/test/java/il/org/spartan/utils/BoolTest.java
@@ -2,28 +2,30 @@ package il.org.spartan.utils;
 
 import org.junit.*;
 
+import fluent.ly.*;
+
 public class BoolTest {
-  @Test @SuppressWarnings("static-method") public void consTest() {
+  @Test @SuppressWarnings({ "static-method", "null" }) public void consTest() {
     final Bool vot = Bool.valueOf(true), cd = new Bool(), vof = Bool.valueOf(false), cf = new Bool(false);
-    assert vot.inner == new Bool(true).inner;
-    assert vot.inner != cd.inner;
-    assert vof.inner == cf.inner;
-    assert vof.inner != vot.inner;
+    azzert.that( vot.inner ,azzert.is( new Bool(true).inner));
+    azzert.that( vot.inner ,azzert.not( cd.inner));
+    azzert.that( vof.inner ,azzert.is( cf.inner));
+    azzert.that( vof.inner ,azzert.not( vot.inner));
   }
 
-  @Test @SuppressWarnings("static-method") public void clearTest() {
+  @Test @SuppressWarnings({ "static-method", "null" }) public void clearTest() {
     final Bool ct = new Bool(true), cf = new Bool(false);
     cf.clear();
-    assert ct.inner != cf.inner;
+    azzert.that( ct.inner ,azzert.not( cf.inner));
     ct.clear();
-    assert ct.inner == cf.inner;
+    azzert.that( ct.inner ,azzert.is( cf.inner));
   }
 
-  @Test @SuppressWarnings("static-method") public void getTest() {
+  @Test @SuppressWarnings({ "static-method", "null" }) public void getTest() {
     final Bool ct1 = new Bool(true), ct2 = new Bool(true), cf1 = new Bool(false), cf2 = new Bool(false);
-    assert ct1.get() != cf1.get();
-    assert ct1.get() == ct2.get();
-    assert cf1.get() == cf2.get();
+    azzert.that( ct1.get(),azzert.not( cf1.get()));
+    azzert.that( ct1.get() ,azzert.is( ct2.get()));
+    azzert.that( cf1.get() ,azzert.is( cf2.get()));
   }
 
   @Test @SuppressWarnings("static-method") public void innerTest() {
@@ -33,15 +35,15 @@ public class BoolTest {
     assert cf1.inner().equals(cf2.inner());
   }
 
-  @Test @SuppressWarnings("static-method") public void setTest() {
+  @Test @SuppressWarnings({ "static-method", "null" }) public void setTest() {
     final Bool ct = new Bool(true), cf = new Bool(false);
     ct.set();
-    assert ct.inner != cf.inner;
+    azzert.that( ct.inner ,azzert.not( cf.inner));
     cf.set();
-    assert ct.inner == cf.inner;
+    azzert.that( ct.inner ,azzert.is( cf.inner));
     cf.set(true);
-    assert ct.inner == cf.inner;
+    azzert.that( ct.inner ,azzert.is( cf.inner));
     cf.set(false);
-    assert ct.inner != cf.inner;
+    azzert.that( ct.inner ,azzert.not( cf.inner));
   }
 }
