b7a9387d557cca1d19c59b9b99f995436f0c8640
HadadIdan <idan.hadad@campus.technion.ac.il>
2018-11-20 22:52:30 +0200

    changed another few things to match lgtm required

--- src/main/java/il/org/spartan/CSVLine.java
+++ src/main/java/il/org/spartan/CSVLine.java
@@ -60,7 +60,7 @@ import il.org.spartan.utils.Separate.*;
   }
 
   /** Adds all {@link External} properties in a given object.
-   * @param ¢ an arbitrary object, usually with some of its fields and methods
+   * @param $ an arbitrary object, usually with some of its fields and methods
    *          marked {@link External}
    * @return the parameter */
   public <T> T extract(final T $) {
