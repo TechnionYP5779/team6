d4c4a23842a67dba7266241a89d7132e1e157c4f
elizabethyeshaayahu320 <44428417+elizabethyeshaayahu320@users.noreply.github.com>
2018-11-21 00:09:24 +0200

    Create zero.java

---
+++ src/main/java/fluent/ly/zero.java
@@ -0,0 +1,22 @@
+package il.org.spartan.etc;
+
+/** Fluent API, see usage
+ * @author Yossi Gil
+ * @since 2017-04-10 */
+@SuppressWarnings("unused") public interface zero {
+  static int forgetting(final Object _1, final Object... _2) {
+    return 0;
+  }
+
+  static int forget(final boolean __) {
+    return 0;
+  }
+
+  static int forget(final double __) {
+    return 0;
+  }
+
+  static int forget(final long __) {
+    return 0;
+  }
+}
