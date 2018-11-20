3298bba0d46baec2cbb937ab85cc97696243719a
Or Yasso <or-yosef@campus.technion.ac.il>
2018-11-20 16:03:11 +0200

    added tests for interface lazy and enum safe. Fixes #111, Fixes #112

---
+++ src/test/java/fluent/ly/lazyTest.java
@@ -0,0 +1,34 @@
+package fluent.ly;
+
+import org.jetbrains.annotations.*;
+import org.junit.*;
+
+import java.util.function.*;
+
+import static fluent.ly.azzert.*;
+
+@SuppressWarnings("static-method") public class lazyTest {
+  @Test public void getTest() {
+    Supplier<@Nullable String> supp = new Supplier<@Nullable String>() {
+      int i;
+
+      @Override public String get() {
+        ++i;
+        return i + "";
+      }
+    };
+    // compute supp.get with the lazy interface, therefore function get happens only
+    // once
+    lazy<@Nullable String> newLazy = lazy.get(supp);
+    azzert.that("1", is(newLazy.get()));
+    azzert.that("1", is(newLazy.get()));
+    // compute supp.get withOut the lazy interface, therefore function get happens
+    // more than once
+    azzert.that("2", is(supp.get()));
+    azzert.that("3", is(supp.get()));
+    // just for sureness compute get with lazy one more time
+    azzert.that("1", is(newLazy.get()));
+    // but i now equal to 3, therefore:
+    azzert.that("4", is(supp.get()));
+  }
+}
---
+++ src/test/java/fluent/ly/safeTest.java
@@ -0,0 +1,12 @@
+package fluent.ly;
+
+import static fluent.ly.azzert.*;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class safeTest {
+  @Test public void doubleDivTest() {
+    azzert.that(box.it(1.0), is(safe.div(1.5, 1.5)));
+    azzert.that(box.it(1.0), is(safe.div(1.5, 0.0)));
+  }
+}
