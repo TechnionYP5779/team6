6ba3198adf0ef3da84af6e90873025e9581d44a9
Or Yasso <or-yosef@campus.technion.ac.il>
2018-11-21 21:47:07 +0200

    removed warnings and spartanized code of faultTest. #134

--- src/test/java/il/org/spartan/utils/faultTest.java
+++ src/test/java/il/org/spartan/utils/faultTest.java
@@ -2,65 +2,40 @@ package il.org.spartan.utils;
 
 import static fluent.ly.azzert.*;
 
-import java.io.*;
-import java.nio.charset.*;
-import java.util.*;
-
-import org.jetbrains.annotations.*;
 import org.junit.*;
 
 import fluent.ly.*;
 
-public class faultTest {  
+@SuppressWarnings({ "static-method", "null" }) public class faultTest {
   @Test public void doneTest() {
-    String expectedOutput = "";
-    expectedOutput += fault.trace(new AssertionError());
-    expectedOutput += "\n   END stack trace: .................]]]\n-----this is all I know.";
+    String expectedOutput = fault.trace(new AssertionError()) + "\n   END stack trace: .................]]]\n-----this is all I know.";
     int indexExpectedOutput = expectedOutput.indexOf("at sun");
-    
-    String finalOutput = "";
-    finalOutput += fault.done();
-    int indexFinalOutput = finalOutput.indexOf("at sun");
-   
-    azzert.that(expectedOutput.substring(indexExpectedOutput), is(finalOutput.substring(indexFinalOutput)));
-  }   
-  
+    String finalOutput = fault.done() + "";
+    azzert.that(expectedOutput.substring(indexExpectedOutput), is(finalOutput.substring(finalOutput.indexOf("at sun"))));
+  }
+
   @Test public void dumpTest() {
     azzert.that("\n FAULT: this should not have happened!\n-----To help you fix the code, here is some info", is(fault.dump()));
   }
-  
+
   @Test public void dump2Test() {
-    String name = "floor";
-    int value = 8;
-    azzert.that( "\n " + name + "=[" + value + "]", is(fault.dump(name,value)));
+    azzert.that("\n floor=[" + 8 + "]", is(fault.dump("floor", box.it(8))));
   }
-  
+
   @Test public void specificallyTest() {
-    String name = "floors";
-    String expectedOutput = "";
-    expectedOutput +="\n" + 
-        " FAULT: this should not have happened!\n" + 
-        " floors\n" + 
-        "-----To help you fix the code, here is some info\n" + 
-        " String=[a]\n" + 
-        " String=[b]\n" + 
-        " String=[c]\n   ";
-    
-    String finalOutput = "";
-    finalOutput += fault.specifically(name, new String[]{"a", "b", "c"});
-    int endWith = finalOutput.indexOf("Stack trace");
-    finalOutput = finalOutput.substring(0, endWith);
-    
+    String name = "floors",
+        expectedOutput = "\n FAULT: this should not have happened!\n floors\n"
+            + "-----To help you fix the code, here is some info\n String=[a]\n String=[b]\n String=[c]\n   ",
+        finalOutput = fault.specifically(name, (Object[]) new String[] { "a", "b", "c" }) + "";
+    finalOutput = finalOutput.substring(0, finalOutput.indexOf("Stack trace"));
     azzert.that(expectedOutput, is(finalOutput));
   }
-  
+
   @Test public void unreachableTest() {
     azzert.that(false, is(fault.unreachable()));
   }
-  
+
   @Test public void boolTest() {
     azzert.that(false, is(fault.bool(null)));
   }
-  
-
 }
