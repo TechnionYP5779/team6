1ae50dcb49c931f62838eddc68c9c60117a80f7e
IlanitSmul <ilanitsmul@campus.technion.ac.il>
2018-11-21 06:06:32 +0200

    close #127 (and increase coverage #38)

---
+++ src/test/java/fluent/ly/allTest.java
@@ -0,0 +1,29 @@
+package fluent.ly;
+
+import java.util.*;
+
+import org.junit.*;
+
+@SuppressWarnings("static-method") public class allTest {
+  @Test public void notNullIterable() {
+    assert !(all.notNull((Iterable<Integer>) null));
+    List<Integer> intlist = new ArrayList<>();
+    assert (all.notNull(intlist));
+    intlist.add(null);
+    assert !(all.notNull(intlist));
+    intlist.add(box.it(3));
+    assert !(all.notNull(intlist));
+    intlist.set(0, box.it(0));
+    assert (all.notNull(intlist));
+  }
+
+  @Test @SuppressWarnings("null") public void notNullArray() {
+    assert !(all.notNull((Integer[]) null));
+    Integer[] intArray = new Integer[3];
+    assert !(all.notNull(intArray));
+    intArray[0] = intArray[1] = box.it(3);
+    assert !(all.notNull(intArray));
+    intArray[2] = box.it(3);
+    assert (all.notNull(intArray));
+  }
+}
