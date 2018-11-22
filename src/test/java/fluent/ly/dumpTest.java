4f882b2cb0f74c5ad0133da8adc2aa55adde2dcd
IlanitSmul <ilanitsmul@campus.technion.ac.il>
2018-11-22 19:14:57 +0200

    issue #132 - add more unit tests for class dump (and increase coverage #38)

--- src/test/java/fluent/ly/dumpTest.java
+++ src/test/java/fluent/ly/dumpTest.java
@@ -4,6 +4,7 @@ import static fluent.ly.azzert.*;
 
 import java.io.*;
 import java.lang.reflect.*;
+import java.net.*;
 import java.util.*;
 
 import org.jetbrains.annotations.*;
@@ -25,90 +26,26 @@ import org.junit.*;
     public void toA() {// can be empty
     }
   }
-  
+
+  PrintStream originalStream = System.out;
+
   @Before public void redirectOutput() {
     newOutPut = new ByteArrayOutputStream();
     System.setOut(new PrintStream(newOutPut));
   }
-/*
+
+  @After public void redirectOutputAgain() {
+    System.setOut(originalStream);
+  }
+
   @Test public void goClassTest() {
     dump.go(SomeMethods.class);
-    
-    azzert.that(
-       "\n" + 
-       "\n" + 
-       "--IDENTIFICATION--\n" + 
-       "Simple Name = SomeMethods\n" + 
-       "Canonical Name = fluent.ly.dumpTest.SomeMethods\n" + 
-       "Name = fluent.ly.dumpTest$SomeMethods\n" + 
-       "toString = class fluent.ly.dumpTest$SomeMethods\n" + 
-       "super class = class java.lang.Object\n" + 
-       "generic super class = class java.lang.Object\n" + 
-       "class = class java.lang.Class\n" + 
-       "No component type\n" + 
-       "class loader = " + SomeMethods.class.getClassLoader() + "\n" + 
-       "--MODIFIERS--\n" + 
-       "Package = package fluent.ly\n" + 
-       "Modifiers (decimal form) = 1\n" + 
-       "Modifiers (binary form) = +1\n" + 
-       "IsSynthetic = false\n" + 
-       "IsPrimitive = false\n" + 
-       "IsFinal = false\n" + 
-       "IsAbstract = false\n" + 
-       "IsStatic = false\n" + 
-       "IsStrictfp = false\n" + 
-       "--Visibility--\n" + 
-       "IsPublic = true\n" + 
-       "IsPrivate = false\n" + 
-       "IsProtected = false\n" + 
-       "--MEMBERS\n" + 
-       "No fields\n" + 
-       "Total of 13 methods:\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.size()\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.getA()\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.isA()\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.toA()\n" + 
-       "\tpublic final void java.lang.Object.wait() throws java.lang.InterruptedException\n" + 
-       "\tpublic final void java.lang.Object.wait(long,int) throws java.lang.InterruptedException\n" + 
-       "\tpublic final native void java.lang.Object.wait(long) throws java.lang.InterruptedException\n" + 
-       "\tpublic boolean java.lang.Object.equals(java.lang.Object)\n" + 
-       "\tpublic java.lang.String java.lang.Object.toString()\n" + 
-       "\tpublic native int java.lang.Object.hashCode()\n" + 
-       "\tpublic final native java.lang.Class java.lang.Object.getClass()\n" + 
-       "\tpublic final native void java.lang.Object.notify()\n" + 
-       "\tpublic final native void java.lang.Object.notifyAll()\n" + 
-       "Only one constructors: public fluent.ly.dumpTest$SomeMethods(fluent.ly.dumpTest)\n" + 
-       "Total of 2 declared fields:\n" + 
-       "\tfinal fluent.ly.dumpTest fluent.ly.dumpTest$SomeMethods.this$0\n" + 
-       "\tprivate static transient boolean[] fluent.ly.dumpTest$SomeMethods.$jacocoData\n" + 
-       "Total of 5 declared methods:\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.size()\n" + 
-       "\tprivate static boolean[] fluent.ly.dumpTest$SomeMethods.$jacocoInit()\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.getA()\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.isA()\n" + 
-       "\tpublic void fluent.ly.dumpTest$SomeMethods.toA()\n" + 
-       "Only one declared constructors: public fluent.ly.dumpTest$SomeMethods(fluent.ly.dumpTest)\n" + 
-       "--CLASS SIGNATURE--\n" + 
-       "No interfaces\n" + 
-       "No annotations\n" + 
-       "No type parameters\n" + 
-       "No declared annotations\n" + 
-       "No generic interfaces\n" + 
-       "--CONTAINERS--\n" + 
-       "No declared classes\n" + 
-       "declaring class = class fluent.ly.dumpTest\n" + 
-       "enclosing class = class fluent.ly.dumpTest\n" + 
-       "No enclosing constructor\n" + 
-       "No enclosing method\n" + 
-       "--CLASS MEMBERS--\n" + 
-       "No public classes\n" + 
-       "No declared classes\n" + 
-       "No declared annotations\n" + 
-       "---------------------------\n" + 
-       "",
-        is(newOutPut + ""));
+    assert (newOutPut + "").contains("\n\n--IDENTIFICATION--\n");
+    assert (newOutPut + "").contains("Simple Name = SomeMethods");
+    assert (newOutPut + "").contains("IsAbstract = false");
+    assert (newOutPut + "").contains("public void fluent.ly.dumpTest$SomeMethods.size()");
   }
-*/
+
   @Test public void goListTest() {
     final ArrayList<Integer> ls = new ArrayList<Integer>();
     ls.add(box.it(1));
@@ -145,5 +82,38 @@ import org.junit.*;
     }
     azzert.that("HELLO\n\n--BEGIN SomeMethods object: " + c1 + "\nClass canonical name = fluent.ly.dumpTest.SomeMethods\n"
         + "Class name = fluent.ly.dumpTest$SomeMethods\n" + dynamicMethodsOrder + "--END OBJECT--\n\n", is(newOutPut + ""));
+  }
+
+  @Test public void goObjectStringParameter() {
+    dump.go("HELLO");
+    assert (newOutPut + "").contains("--BEGIN String object: HELLO");
+    assert (newOutPut + "").contains("Class canonical name = java.lang.String");
+    assert (newOutPut + "").contains("--END OBJECT--");
+  }
+
+  @Test public void goObjectNull() {
+    dump.go((Object) null, "stringA");
+    assert (newOutPut + "").contains("stringA");
+    assert (newOutPut + "").contains("NULL");
+  }
+
+  @Test public void goArray() {
+    Object[] arr = new Object[3];
+    arr[0] = "1";
+    arr[0] = "2";
+    arr[0] = "3";
+    dump.go(arr);
+    assert (newOutPut + "").contains("Total of 3 elements");
+  }
+
+  @Test public void goArrayAndMoreParameters() {
+    Object[] arr = new Object[3];
+    arr[0] = "1";
+    arr[0] = "2";
+    arr[0] = "3";
+    dump.go(arr, "stringA", "stringB");
+    assert (newOutPut + "").contains("stringA");
+    assert (newOutPut + "").contains("stringB");
+    assert (newOutPut + "").contains("Total of 3 elements");
   }
 }
