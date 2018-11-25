88fbd5299e221acc9dd839fe8f530e2ca5e07db9
MichaelShohat <38256261+MichaelShohat@users.noreply.github.com>
2018-11-25 16:17:36 +0200

    working on #141

--- src/test/java/fluent/ly/azzertTest.java
+++ src/test/java/fluent/ly/azzertTest.java
@@ -26,6 +26,73 @@ import fluent.ly.azzert;
     azzert.assertCollectionsEqual("fail", l1, arr);
     azzert.assertCollectionsEqual("fail", arr, l2);
   }
+
+  @Test(expected = AssertionError.class) public void azzertCollectionNotEqualTest1() {
+    ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    ArrayList<Integer> l2 = new ArrayList<>(); // 1,4
+    l1.add(box.it(1));
+    l2.add(box.it(1));
+    l1.add(box.it(4));
+    l2.add(box.it(4));
+    l1.add(box.it(9));
+    azzert.assertCollectionsEqual(l1, l2);
+  }
+
+  @Test(expected = AssertionError.class) public void azzertCollectionNotEqualTest2() {
+    ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    ArrayList<Integer> l2 = new ArrayList<>(); // 1,4
+    l1.add(box.it(1));
+    l2.add(box.it(1));
+    l1.add(box.it(4));
+    l2.add(box.it(4));
+    l1.add(box.it(9));
+    azzert.assertCollectionsEqual(l2, l1);
+  }
+
+  @Test(expected = AssertionError.class) public void azzertCollectionNotEqualTest3() {
+    ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    Integer[] arr = new Integer[3]; // 1,4,8
+    l1.add(box.it(1));
+    l1.add(box.it(4));
+    l1.add(box.it(9));
+    arr[0] = box.it(1);
+    arr[1] = box.it(4);
+    arr[2] = box.it(8);
+    azzert.assertCollectionsEqual(l1, arr);
+  }
+
+  @Test(expected = AssertionError.class) public void azzertCollectionNotEqualTest4() {
+    ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    ArrayList<Integer> l2 = new ArrayList<>(); // 1,4
+    l1.add(box.it(1));
+    l2.add(box.it(1));
+    l1.add(box.it(4));
+    l2.add(box.it(4));
+    l1.add(box.it(9));
+    azzert.assertCollectionsEqual("fail", l1, l2);
+  }
+
+  @Test(expected = AssertionError.class) public void azzertCollectionNotEqualTest5() {
+    ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    Integer[] arr = new Integer[2]; // 1,4
+    l1.add(box.it(1));
+    l1.add(box.it(4));
+    l1.add(box.it(9));
+    arr[0] = box.it(1);
+    arr[1] = box.it(4);
+    azzert.assertCollectionsEqual("fail", arr, l1);
+  }
   
-  
+  @Test(expected = AssertionError.class) public void azzertCollectionNotEqualTest6() {
+    ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    Integer[] arr = new Integer[4]; // 1,4,5,6
+    l1.add(box.it(1));
+    l1.add(box.it(4));
+    l1.add(box.it(9));
+    arr[0] = box.it(1);
+    arr[1] = box.it(4);
+    arr[2] = box.it(5);
+    arr[3] = box.it(6);
+    azzert.assertCollectionsEqual("fail", l1, arr);
+  }
 }
