6e5e3b48868e2f22ee4330522b7d5f9f887476e8
MichaelShohat <38256261+MichaelShohat@users.noreply.github.com>
2018-11-25 16:42:21 +0200

    working on #141

--- src/test/java/fluent/ly/azzertTest.java
+++ src/test/java/fluent/ly/azzertTest.java
@@ -93,4 +93,57 @@ import org.junit.*;
     arr[3] = box.it(6);
     azzert.assertCollectionsEqual("fail", l1, arr);
   }
+
+  @Test public void azzertContainsTest() {
+    final ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    l1.add(box.it(1));
+    l1.add(box.it(4));
+    l1.add(box.it(9));
+    azzert.assertContains(l1, box.it(1));
+    azzert.assertContains(l1, box.it(4));
+    azzert.assertContains(l1, box.it(9));
+    azzert.assertContains("fail", l1, box.it(1));
+    azzert.assertContains("fail", l1, box.it(4));
+    azzert.assertContains("fail", l1, box.it(9));
+  }
+
+  @Test(expected = AssertionError.class) public void azzertContainsFailTest1() {
+    final ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    l1.add(box.it(1));
+    l1.add(box.it(4));
+    l1.add(box.it(9));
+    azzert.assertContains(l1, box.it(10));
+  }
+
+  @Test(expected = AssertionError.class) public void azzertContainsFailTest2() {
+    final ArrayList<Integer> l1 = new ArrayList<>(); // 1,4,9
+    l1.add(box.it(1));
+    l1.add(box.it(4));
+    l1.add(box.it(9));
+    azzert.assertContains("fail", l1, box.it(10));
+  }
+
+  @Test public void azzertAssertEqualsTest1() {
+    final Boolean t = box.it(true);
+    final Boolean f = box.it(false);
+    azzert.assertEquals(true, true);
+    azzert.assertEquals(false, false);
+    azzert.assertEquals(t, true);
+    azzert.assertEquals(f, false);
+    azzert.assertEquals(true, t);
+    azzert.assertEquals(false, f);
+    azzert.assertEquals("fail", true, true);
+    azzert.assertEquals("fail", t, true);
+    azzert.assertEquals("fail", true, t);
+  }
+
+  @Test public void azzertAssertEqualsTest2() {
+    final Integer b_one = box.it(1);
+    azzert.assertEquals(1, 1);
+    azzert.assertEquals(1, b_one);
+    azzert.assertEquals(b_one, 1);
+    azzert.assertEquals("fail", 1, 1);
+    azzert.assertEquals("fail", 1, b_one);
+    azzert.assertEquals("fail", b_one, 1);
+  }
 }
