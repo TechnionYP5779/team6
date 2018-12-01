57dff97283e204eafd823be651aab9e01a7e1e21
Nitzan810 <44426817+Nitzan810@users.noreply.github.com>
2018-12-01 17:39:21 +0200

    Finished class Time including tests, closes #158

--- src/main/java/parking/Time.java
+++ src/main/java/parking/Time.java
@@ -1,4 +1,107 @@
 package parking;
 
+/** Class representing an interval of time in a week. Includes day of the week
+ * and the range of hours and minutes.
+ * @Package: parking
+ * @Since: 1 בדצמ׳ 2018
+ * @Author: user */
 public class Time {
+  private final WeekDay day;
+  private final DayTime fromHour;
+  private final DayTime toHour;
+  private static final int minDistance = 60;
+
+  /** Class used to store a time in the day, only contains hour and minute
+   * components. Immutable.
+   * @Package: parking
+   * @Since: 29 בנוב׳ 2018
+   * @Author: Nitzan */
+  public static class DayTime {
+    private final int minute;
+    private final int hour;
+    private static final int minutesPerHour = 60;
+
+    public DayTime(final int hour, final int minute) {
+      if (minute < 0 || minute > 59 || hour < 0 || hour > 23)
+        throw new IllegalArgumentException();
+      this.minute = minute;
+      this.hour = hour;
+    }
+
+    public int getHour() {
+      return hour;
+    }
+
+    public int getMinute() {
+      return minute;
+    }
+
+    int compareTo(final DayTime ¢) {
+      return this.hour < ¢.hour ? -1 : this.hour > ¢.hour ? 1 : this.minute < ¢.minute ? -1 : this.minute > ¢.minute ? 1 : 0;
+    }
+
+    boolean isEarlierThan(final DayTime ¢) {
+      return this.compareTo(¢) == -1;
+    }
+
+    boolean isLaterThan(final DayTime ¢) {
+      return this.compareTo(¢) == 1;
+    }
+
+    int diffInMinutes(final DayTime ¢) {
+      return this.getMinute() + minutesPerHour * (this.getHour() - ¢.getHour()) - ¢.getMinute();
+    }
+
+    @Override public boolean equals(final Object ¢) {
+      if (¢ == this)
+        return true;
+      return ¢ instanceof DayTime && compareTo((DayTime) ¢) == 0;
+    }
+
+    @Override public int hashCode() {
+      return minute + 100 * hour;
+    }
+  }
+  // End of class DayTime
+
+  public enum WeekDay {
+    Sunday, Monday, Tuesday, Wednesday, Thursay, Friday, Saturday
+  }
+
+  public Time(final WeekDay day, final DayTime from, final DayTime to) {
+    if (!from.isEarlierThan(to))
+      throw new IllegalArgumentException();
+    this.day = day;
+    this.fromHour = from;
+    this.toHour = to;
+  }
+
+  public WeekDay getDay() {
+    return day;
+  }
+
+  public DayTime getFromHour() {
+    return fromHour;
+  }
+
+  public DayTime getToHour() {
+    return toHour;
+  }
+
+  public boolean isConflicting(final Time ¢) {
+    if (¢.getDay() != this.getDay())
+      return false;
+    if (this.getFromHour().isEarlierThan(¢.getFromHour()) && ¢.getFromHour().isEarlierThan(this.getToHour())
+        || ¢.getFromHour().isEarlierThan(this.getFromHour()) && this.getFromHour().isEarlierThan(¢.getToHour()))
+      return true;
+    int diff = Math.abs(¢.getToHour().diffInMinutes(this.getFromHour()));
+    if (diff < minDistance)
+      return true;
+    diff = Math.abs(this.getToHour().diffInMinutes(¢.getFromHour()));
+    return diff < minDistance;
+  }
+
+  public int lengthInMinutes() {
+    return toHour.diffInMinutes(fromHour);
+  }
 }
---
+++ src/test/java/parking/TimeTest.java
@@ -0,0 +1,135 @@
+package parking;
+
+import static fluent.ly.azzert.*;
+
+import org.junit.*;
+
+import fluent.ly.*;
+import parking.Time.*;
+
+/**
+ * Unit tests for class Time and it's subclass DayTime.
+ * 
+ * @Package: parking
+ * @Since: 1 בדצמ׳ 2018
+ * @Author: Nitzan
+ * @see Time
+ */
+@SuppressWarnings("static-method")
+public class TimeTest {
+  
+  @Test
+  public void testDayTimeRestrictions() {
+    Time.DayTime $;
+    try {
+      $ = new Time.DayTime(-1,30);
+    }
+    catch(IllegalArgumentException ¢) {
+      azzert.that(¢.getClass(), is(new IllegalArgumentException().getClass()));
+    }
+    try {
+     $ = new Time.DayTime(20, -1);
+    }
+    catch(IllegalArgumentException ¢) {
+      azzert.that(¢.getClass(), is(new IllegalArgumentException().getClass()));
+    }
+    try {
+      $ = new Time.DayTime(24, 30);
+    }
+    catch(IllegalArgumentException ¢) {
+      azzert.that(¢.getClass(), is(new IllegalArgumentException().getClass()));
+    }
+    try {
+      $ = new Time.DayTime(20, 60);
+    }
+    catch(IllegalArgumentException ¢) {
+      azzert.that(¢.getClass(), is(new IllegalArgumentException().getClass()));
+    }
+    $ = new Time.DayTime(20, 40);
+    azzert.that($.getHour(), is(20));
+    azzert.that($.getMinute(), is(40));
+  }
+  
+  @Test
+  public void testCompare() {
+    azzert.that(new Time.DayTime(2, 30).compareTo(new Time.DayTime(3, 30)), is(-1));
+    azzert.that(new Time.DayTime(4, 30).compareTo(new Time.DayTime(3, 30)), is(1));
+    azzert.that(new Time.DayTime(2, 30).compareTo(new Time.DayTime(2, 40)), is(-1));
+    azzert.that(new Time.DayTime(2, 50).compareTo(new Time.DayTime(2, 40)), is(1));
+    azzert.that(new Time.DayTime(2, 30).compareTo(new Time.DayTime(2, 30)), is(0));
+  }
+  
+  @Test @SuppressWarnings("unlikely-arg-type") 
+  public void testDayTimeEquals() {
+    Time.DayTime $ = new Time.DayTime(1, 30);
+    assert $.equals(new Time.DayTime(1, 30));
+    assert !$.equals(new Time.DayTime(1, 50));
+    assert $.equals($);
+    assert !$.equals(box.boxThis(80085));
+    
+    azzert.that($.hashCode(), is(130));
+  }
+  
+  @Test
+  public void testRelativityMethods() {
+    assert new Time.DayTime(2, 30).isEarlierThan(new Time.DayTime(2, 40));
+    assert !new Time.DayTime(2, 50).isEarlierThan(new Time.DayTime(2, 40));
+    assert !new Time.DayTime(2, 30).isEarlierThan(new Time.DayTime(2, 30));
+    
+    assert new Time.DayTime(2, 30).isLaterThan(new Time.DayTime(2, 20));
+    assert !new Time.DayTime(2, 30).isLaterThan(new Time.DayTime(2, 40));
+    assert !new Time.DayTime(2, 30).isLaterThan(new Time.DayTime(2, 30));
+  }
+  
+  @Test
+  public void testDiffInMinutes() {
+    azzert.that(new Time.DayTime(5, 30).diffInMinutes(new Time.DayTime(5, 20)), is(10));
+    azzert.that(new Time.DayTime(5, 30).diffInMinutes(new Time.DayTime(5, 40)), is(-10));
+    azzert.that(new Time.DayTime(7, 30).diffInMinutes(new Time.DayTime(5, 30)), is(120));
+    azzert.that(new Time.DayTime(7, 30).diffInMinutes(new Time.DayTime(10, 30)), is(-180));
+    azzert.that(new Time.DayTime(5, 30).diffInMinutes(new Time.DayTime(4, 50)), is(40));
+    azzert.that(new Time.DayTime(7, 30).diffInMinutes(new Time.DayTime(8, 50)), is(-80));
+  }
+  
+  @Test
+  public void testTimeRestrictions() {
+    Time $;
+    try {
+      $ = new Time(WeekDay.Sunday,new Time.DayTime(5,10),new Time.DayTime(5, 5));
+    }
+    catch(IllegalArgumentException ¢) {
+      azzert.that(¢.getClass(), is(new IllegalArgumentException().getClass()));
+    }
+    try {
+      $ = new Time(WeekDay.Monday,new Time.DayTime(1,1),new Time.DayTime(1, 1));
+    }
+    catch(IllegalArgumentException ¢) {
+      azzert.that(¢.getClass(), is(new IllegalArgumentException().getClass()));
+    }
+    $ = new Time (WeekDay.Tuesday,new Time.DayTime(2, 0),new Time.DayTime(4, 0));
+    azzert.that($.lengthInMinutes(), is(120));
+    azzert.that($.getDay(), is(WeekDay.Tuesday));
+    azzert.that($.getFromHour(), is(new Time.DayTime(2, 0)));
+    azzert.that($.getToHour(), is(new Time.DayTime(4, 0)));
+  }
+  
+  @Test
+  public void testConficting() {
+    Time $ = new Time(WeekDay.Friday,new Time.DayTime(6, 0),new Time.DayTime(8, 20));
+    assert !$.isConflicting(new Time(WeekDay.Sunday,new Time.DayTime(6, 0),new Time.DayTime(8, 20)));
+    
+    assert $.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(7, 0),new Time.DayTime(9, 20)));
+    assert $.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(5, 0),new Time.DayTime(8, 0)));
+    
+    assert $.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(5, 0),new Time.DayTime(5, 40)));
+    assert $.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(8, 50),new Time.DayTime(9, 10)));
+    
+    assert !$.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(2, 50),new Time.DayTime(4, 10)));
+    assert !$.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(9, 50),new Time.DayTime(10, 10)));
+    
+    assert !$.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(9, 20),new Time.DayTime(10, 10)));
+    assert !$.isConflicting(new Time(WeekDay.Friday,new Time.DayTime(4, 0),new Time.DayTime(5, 0)));
+  }
+  
+  
+}
