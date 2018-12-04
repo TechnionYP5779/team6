ba502e44ac89cae2ec25a43b5d9bd8c66a980c72
HadadIdan <idan.hadad@campus.technion.ac.il>
2018-12-04 14:50:36 +0200

    refactored project's code template Added few custom Javadoc tags. Generated Java Doc and ant-script for it. Added a wiki page about posts requests: https://github.com/TechnionYP5779/team6/wiki/Post-Request-Info-and-Conventions Created a simple Post Handler to test new defined conventions, close #167

--- src/main/java/server/OurServer.java
+++ src/main/java/server/OurServer.java
@@ -1,10 +1,8 @@
 package server;
-
 import java.io.*;
 import java.net.*;
 
 import com.sun.net.httpserver.*;
-
 public class OurServer {
   static int port = 8000;
 
@@ -16,11 +14,40 @@ public class OurServer {
       x.getResponseBody().close();
     }
   }
+  // TODO: implement filter mechanism against wrong requests
+  /**
+   * 
+   * @fluent.ly.Package server
+   * @fluent.ly.Since 4 Dec 2018
+   * @fluent.ly.Author Idan Hadad
+   */
+  public static class PostHandler implements HttpHandler {
+    @Override public void handle(final HttpExchange x) throws IOException {
+    //this handler right now should only pare post requests
+      if(!"POST".equals(x.getRequestMethod())) 
+        return;
+      //I expect to recieve a header  with the client name this test
+      if(!x.getRequestHeaders().containsKey("client_name"))
+        return;
+      String clName = x.getRequestHeaders().getFirst("client_name");
+      byte[] body= new byte[256]; //expected input max sized 256 bytes
+      if (x.getRequestBody().read(body) == -1) //couldn't read
+          return;
+      //body should now contain a messege which i will "encrypt"
+      String response = "<h1>Client name is:" + clName + "</h1><h1> Encrypted input is:" + new StringBuilder(body + "").reverse() + "</h1>";
+      x.sendResponseHeaders(444, response.getBytes().length);
+      x.getResponseBody().write(response.getBytes());
+      x.getResponseBody().close();
+      
+    }
+  }
 
   public static void main(final String[] args) throws Exception {
     final HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
     server.createContext("/", new RootHandler());
+    server.createContext("/postTest", new PostHandler());
     server.setExecutor(null);
     server.start();
+   
   }
 }
