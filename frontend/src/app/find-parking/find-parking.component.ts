82c2d8a54d394a28a3e62c38c0954fcdb54a1379
eyal <eyalf210@gmail.com>
2018-12-05 10:30:21 +0200

    added find-parking component. closes #180

---
+++ frontend/src/app/find-parking/find-parking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-find-parking',
+  templateUrl: './find-parking.component.html',
+  styleUrls: ['./find-parking.component.css']
+})
+export class FindParkingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
