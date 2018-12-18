45edaf5f588ed4f2df6aa8499670fdf2980348c6
HadadIdan <idan.hadad@campus.technion.ac.il>
2018-12-18 23:59:00 +0200

    connected post requests to MySQL

--- src/main/java/database/DBMain.java
+++ src/main/java/database/DBMain.java
@@ -3,61 +3,39 @@ package database;
 import java.sql.*;
 
 public class DBMain {
-  
-  static final String createParkingSpotsTable =  
-      "CREATE TABLE ParkingSpots (\n" +
-          "id INT NOT NULL AUTO_INCREMENT,\n" +
-          "price INT NOT NULL,\n" +
-          "city VARCHAR(20) NOT NULL,\n" +
-          "street VARCHAR(20) NOT NULL,\n" +
-          "building INT NOT NULL,\n" +
-          "owner INT NOT NULL,\n" +
-          "buyer INT,\n" +
-          "PRIMARY KEY(id));";
-  
+  static final String createParkingSpotsTable = "CREATE TABLE ParkingSpots (\nid INT NOT NULL AUTO_INCREMENT,\nprice INT NOT NULL,\n"
+      + "city VARCHAR(20) NOT NULL,\nstreet VARCHAR(20) NOT NULL,\nbuilding INT NOT NULL,\nstartTime VARCHAR(10) NOT NULL,\n"
+      + "endTime VARCHAR(10) NOT NULL,\nowner INT NOT NULL,\nbuyer INT,\nPRIMARY KEY(id));";
   static final String destroyParkingSpotsTable = "DROP TABLE ParkingSpots;";
-  
-  public static void main(String[] args) {
-    //addParkingSpot(10,"Tel Aviv","Street",10,50,40);
+
+  public static void main(final String[] args) {
+    // addParkingSpot(10,"Tel Aviv","Street",10,50,40);
     printAllSpots();
     System.out.println("Done");
   }
-  
-  public static void addParkingSpot(int price, String city, String street, int building, int owner, int buyer) {
+
+  public static void addParkingSpot(final int price, final String city, final String street, final int building, final int owner, final int buyer,
+      final String startTime, final String endTime) {
     try {
-      SQLUtils.runCommand(
-          "INSERT INTO ParkingSpots (price,city,street,building,owner,buyer)\n" +
-          "VALUES ("+price+",'"+city+"','"+street+"',"+building+","+owner+","+buyer+");"
-          );
-    }
-    catch(SQLException ¢) {
+      SQLUtils.runCommand("INSERT INTO ParkingSpots (price,city,street,building,startTime,endTime,owner,buyer)\nVALUES (" + price + ",'" + city
+          + "','" + street + "'," + building + ",'" + startTime + "','" + endTime + "'," + owner + "," + buyer + ");");
+    } catch (final SQLException ¢) {
       System.out.println(¢);
     }
   }
-  
+
   public static void printAllSpots() {
     QueryResults q = null;
     try {
       q = SQLUtils.runQuery("SELECT * FROM ParkingSpots;");
-      @SuppressWarnings("resource") ResultSet r = q.getResults();
-      while(r.next())
-        System.out.println(
-            "ID: "+r.getInt("id")+"\t"+
-            "City: "+r.getString("city")+"\t"+
-            "Street: "+r.getString("street")+"\t"+
-            "Building: "+r.getInt("building")+"\t"+
-            "Owner: "+r.getInt("owner")+"\t"+
-            "Buyer: "+r.getInt("buyer")
-        );
-    }
-    catch(SQLException ¢) {
+      for (final ResultSet ¢ = q.getResults(); ¢.next();)
+        System.out.println("ID: " + ¢.getInt("id") + "\tCity: " + ¢.getString("city") + "\tStreet: " + ¢.getString("street") + "\tBuilding: "
+            + ¢.getInt("building") + "\tOwner: " + ¢.getInt("owner") + "\tBuyer: " + ¢.getInt("buyer"));
+    } catch (final SQLException ¢) {
       System.out.println(¢);
-    }
-    finally {
-      if(q!=null)
+    } finally {
+      if (q != null)
         q.close();
     }
   }
-  
-  
 }
