22d3851648c78787e2c61eb0477d31ea7b055ad2
HadadIdan <idan.hadad@campus.technion.ac.il>
2018-12-18 22:10:56 +0200

    added very simple post req to server

--- src/main/java/com/auth0/example/LetSpotServlet.java
+++ src/main/java/com/auth0/example/LetSpotServlet.java
@@ -7,14 +7,13 @@ import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 
-//import org.json.*;
-
+// import org.json.*;
 @WebServlet(urlPatterns = { "/add/renting_spot" }) @SuppressWarnings("serial") public class LetSpotServlet extends HttpServlet {
-  @Override protected void doPost(HttpServletRequest r, HttpServletResponse resp) throws ServletException, IOException {
+  @Override protected void doPost(final HttpServletRequest r, final HttpServletResponse resp) throws ServletException, IOException {
     if (!"POST".equals(r.getMethod()))// should only be used for Post Requests
       return;
-    String body = r.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-  //  System.getInstance().add(new JSONObject(new String(body)));
+    final String body = r.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+    // System.getInstance().add(new JSONObject(new String(body)));
     resp.setHeader("Response", "OK");
     resp.getWriter().write("JSON recieved: " + body);
   }
