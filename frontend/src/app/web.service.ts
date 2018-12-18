dab6f8fda3d41cad874b169af8d87a7cf02647da
eyal <eyalf210@gmail.com>
2018-12-19 00:07:33 +0200

    added post request

--- frontend/src/app/web.service.ts
+++ frontend/src/app/web.service.ts
@@ -2,19 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient , HttpHeaders } from '@angular/common/http';
 
 
-
-
 const httpOptions = {
   headers: new HttpHeaders({
-    'Access-Control-Allow-Origin':  'http://localhost:8000/*'
+    'Access-Control-Allow-Origin': 'http://localhost:8080/add/renting_spot'
   }),
 };
 
 @Injectable()
 export class WebService {
 
-	BASE_URL = 'http://localhost:8000';
+BASE_URL = 'http://localhost:8080';
+ADD_SPOT_URL = '/add/renting_spot';
+
+	
   	constructor(private http: HttpClient) { }
+
+  	addSpot(rent){
+  		this.http.post(this.BASE_URL+this.ADD_SPOT_URL, rent).subscribe(res=>{
+  			console.log(JSON.stringify(res));	
+  			alert("successfully add a new spot")
+  		},
+  		err=>{
+  			alert("an error occurred. please try again")
+  		}
+  		);
+  	}
 
 
 
