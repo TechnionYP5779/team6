5923fd2b2c475e07a2bbdb11a8454a2f34810715
IlanitSmul <ilanitsmul@campus.technion.ac.il>
2018-12-19 12:04:39 +0200

    GUI: update rent spot form - add reset button, use angular material forms, add date field

--- frontend/src/app/rent-spot-form/rent-spot-form.component.ts
+++ frontend/src/app/rent-spot-form/rent-spot-form.component.ts
@@ -1,67 +1,85 @@
 import { Component, OnInit } from '@angular/core';
-import {RentSpot} from '../rent-spot';
+import { RentSpotModel } from '../rent-spot'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { WebService } from '../web.service';
 
 @Component({
-  selector: 'app-rent-spot-form',
-  templateUrl: './rent-spot-form.component.html',
-  styleUrls: ['./rent-spot-form.component.css']
+	selector: 'app-rent-spot-form',
+	templateUrl: './rent-spot-form.component.html',
+	styleUrls: ['./rent-spot-form.component.css']
 })
 export class RentSpotFormComponent implements OnInit {
 
- 	Hours = [
-			' ' ,
-		  	'00:00' ,
-			'01:00' ,
-			'02:00' ,
-			'03:00' ,
-			'04:00' ,
-			'05:00' ,
-			'06:00' ,
-			'07:00' ,
-			'08:00' ,
-			'09:00' ,
-			'10:00' ,
-			'11:00' ,
-			'12:00' ,
-			'13:00' ,
-			'14:00' ,
-			'15:00' ,
-			'16:00' ,
-			'17:00'	,
-			'18:00' ,
-			'19:00' ,
-			'20:00' ,
-			'21:00' ,
-			'22:00' ,
-			'23:00' ,
-			'24:00' 
-			]
+	rentSpotForm: FormGroup;
 
-	submitted = false
+	rentSpotModel: RentSpotModel = new RentSpotModel('', '', null, null, 0)
 
-	model = new RentSpot('','','','',0)
+	constructor(private fb: FormBuilder, private webService: WebService) {
+		this.rentSpotForm = fb.group({
+			floatLabel: 'auto',
+			'city': ["",
+				[Validators.required,
+				Validators.pattern('[a-zA-Z ]*')]
+			],
+			'street': ["",
+				[Validators.required,
+				Validators.pattern('[a-zA-Z ]*')]
+			],
+			'spot_num': ["",
+				[Validators.pattern('[0-9]*')]
+			],
 
- 	constructor(private webService : WebService ) { }
+			'start_date': ["",
+				[Validators.required]
+			],
+			'start_hour': ["",
+				[Validators.required]
+			],
 
- 	// constructor( ) { }
+			'end_date': ["",
+				[Validators.required]
+			],
+			'end_hour': ["",
+				[Validators.required]
+			],
 
-  	ngOnInit() { }
-
-	onSubmit(){ 
-		this.submitted = true
-	}		
-
-	get diagnostic() {
-	 	return JSON.stringify(this.model); 
+			'price': ["",
+				[Validators.required,
+				Validators.pattern('[0-9]+((.)[0-9]+)?')]
+			],
+		});
 	}
 
-	 newSpot(){
-		// fill with a post request
-		console.log("adding new spot");
-		this.webService.addSpot(JSON.stringify(this.model))
-		// console.log(JSON.stringify(x));
+	ngOnInit() { }
+
+	addNewSpot() {
+		this.rentSpotModel.city = this.rentSpotForm.value.city
+		this.rentSpotModel.street = this.rentSpotForm.value.street
+		this.rentSpotModel.spot_num = this.rentSpotForm.value.spot_num
+
+		var startDate = new Date(this.rentSpotForm.value.start_date);
+		var startHourString = this.rentSpotForm.value.start_hour.split(':'); // [0]=hour, [1]=min
+		var startTime = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate(), startHourString[0], startHourString[1])
+
+		var endDate = new Date(this.rentSpotForm.value.end_date);
+		var endHourString = this.rentSpotForm.value.end_hour.split(':'); // [0]=hour, [1]=min
+		var endTime = new Date(endDate.getFullYear(), endDate.getMonth(), endDate.getDate(), endHourString[0], endHourString[1])
+
+		this.rentSpotModel.start_time = startTime
+		this.rentSpotModel.end_time = endTime
+
+		this.rentSpotModel.price = this.rentSpotForm.value.price
+
+		this.reset();
+		console.log("The rent spot form was submitted: " + JSON.stringify(this.rentSpotModel))  // TODO: delete!
+		this.webService.addSpot(JSON.stringify(this.rentSpotModel))
+
+		// for tests:
+		// console.log("time: " + " yyyy= " + sd.getFullYear() + " mm= " + sd.getMonth() + " dd= " + sd.getDate() + " h= " + sd.getHours() + " m= " + sd.getMinutes())
+	}
+
+	reset() {
+		this.rentSpotForm.reset();
 	}
 
 }
-
