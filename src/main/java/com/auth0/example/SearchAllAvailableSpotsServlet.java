4310cc18a6192452ca13afa9a3105be52c55e69f
HadadIdan <idan.hadad@campus.technion.ac.il>
2018-12-26 03:00:11 +0200

    added userId to needed jsons

--- src/main/java/com/auth0/example/SearchAllAvailableSpotsServlet.java
+++ src/main/java/com/auth0/example/SearchAllAvailableSpotsServlet.java
@@ -2,29 +2,29 @@ package com.auth0.example;
 
 import java.io.*;
 
-import java.util.stream.*;
-
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
-import parking.OurSystem;
 
- import org.json.*;
-@WebServlet(urlPatterns = { "/logged/search/all/renting_spots" }) @SuppressWarnings("serial") public class SearchAllAvailableSpotsServlet extends HttpServlet {
+import org.json.*;
+
+import parking.*;
+
+@WebServlet(urlPatterns = { "/logged/search/all/renting_spots" }) @SuppressWarnings("serial") public class SearchAllAvailableSpotsServlet
+    extends HttpServlet {
   @Override protected void doPost(final HttpServletRequest r, final HttpServletResponse resp) throws ServletException, IOException {
     if (!"POST".equals(r.getMethod()))// should only be used for Post Requests
       return;
-    final String body = r.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-    resp.setHeader("Access-Control-Allow-Origin","*");
+    resp.setHeader("Access-Control-Allow-Origin", "*");
     String psList = "";
     try {
       psList = OurSystem.getAllAvailableParkingSpots() + "";
-    } catch ( JSONException ¢) {
+    } catch (final JSONException ¢) {
       resp.setHeader("Response", "ERROR");
       resp.getWriter().write(new JSONObject().put("Desc", ¢ + "") + "");
       return;
     }
-    if(psList.equals(null)) {
+    if (psList.equals(null)) {
       resp.setHeader("Response", "ERROR");
       resp.getWriter().write(new JSONObject().put("Desc", "Couldn't parse from JSONObject to string") + "");
       return;
