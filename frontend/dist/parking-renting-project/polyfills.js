ab8e9a7af4b182ec8b78a8121251f09b131a496b
MichaelShohat <michaelsho@campus.technion.ac.il>
2018-12-26 21:32:16 +0200

    working on #252 - need to add number of total and free spots

--- frontend/dist/parking-renting-project/polyfills.js
+++ frontend/dist/parking-renting-project/polyfills.js
@@ -5497,8 +5497,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/eyal/Documents/school/semester_6/yearly_project/project11/team6/frontend/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/eyal/Documents/school/semester_6/yearly_project/project11/team6/frontend/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\micha\Documents\Git\team6\frontend\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\micha\Documents\Git\team6\frontend\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
