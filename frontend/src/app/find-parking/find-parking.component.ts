ee19025ce1d0a0dcea87107a982e4e80f4bc32a5
eyal <eyalf210@gmail.com>
2018-12-28 00:31:44 +0200

    fixed address filter

--- frontend/src/app/find-parking/find-parking.component.ts
+++ frontend/src/app/find-parking/find-parking.component.ts
@@ -205,9 +205,9 @@ export class FindParkingComponent implements OnInit {
 
       // if (((spot.distance <= this.filterElement.maxDistance) || (this.filterElement.maxDistance == -1)) &&
 
-      if (((spot.price <= this.filterElement.maxPrice) || (this.filterElement.maxPrice == -1))) {
-        this.ELEMENT_DATA_FILTER.push(spot);
-      }
+      // if (((spot.price <= this.filterElement.maxPrice) || (this.filterElement.maxPrice == -1))) {
+      //   this.ELEMENT_DATA_FILTER.push(spot);
+      // }
 
     }
 
